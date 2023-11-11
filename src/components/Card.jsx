45bc5bb9e29079b2fbfa6adc1bb9fafd2698817b
SUJAY GOWDA <sujay.gowda@i4sight.net>
2023-11-11 20:26:12 +0530

    employee select state uplift and css styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,110 +1,70 @@
-const employees = [
-  {
-    name: 'John Doe',
-    joiningDate: '2023-01-01',
-    employeeCode: 'EMP001',
-    phoneNumber: '123-456-7890',
-    designation: 'Software Engineer',
-    department: 'Engineering',
-    emailAddress: 'john.doe@example.com',
-    image: 'https://i.pravatar.cc/48?u=499476',
-  },
-  {
-    name: 'Jane Smith',
-    joiningDate: '2023-02-15',
-    employeeCode: 'EMP002',
-    phoneNumber: '234-567-8901',
-    designation: 'Product Manager',
-    department: 'Management',
-    emailAddress: 'jane.smith@example.com',
-    image: 'https://i.pravatar.cc/48?u=499476',
-  },
-  {
-    name: 'Bob Johnson',
-    joiningDate: '2023-03-10',
-    employeeCode: 'EMP003',
-    phoneNumber: '345-678-9012',
-    designation: 'Graphic Designer',
-    department: 'Design',
-    emailAddress: 'bob.johnson@example.com',
-    image: 'https://i.pravatar.cc/48?u=499476',
-  },
-  {
-    name: 'Emily Davis',
-    joiningDate: '2023-04-20',
-    employeeCode: 'EMP004',
-    phoneNumber: '456-789-0123',
-    designation: 'HR Manager',
-    department: 'HR Department',
-    emailAddress: 'emily.davis@example.com',
-    image: 'https://i.pravatar.cc/48?u=499476',
-  },
-  {
-    name: 'Alex Turner',
-    joiningDate: '2023-05-05',
-    employeeCode: 'EMP005',
-    phoneNumber: '567-890-1234',
-    designation: 'Marketing Specialist',
-    department: 'Marketing',
-    emailAddress: 'alex.turner@example.com',
-    image: 'https://i.pravatar.cc/48?u=499476',
-  },
-  {
-    name: 'Sara Miller',
-    joiningDate: '2023-06-15',
-    employeeCode: 'EMP006',
-    phoneNumber: '678-901-2345',
-    designation: 'Financial Analyst',
-    department: 'Finance',
-    emailAddress: 'sara.miller@example.com',
-    image: 'https://i.pravatar.cc/48?u=499476',
-  },
-  {
-    name: 'Chris White',
-    joiningDate: '2023-07-25',
-    employeeCode: 'EMP007',
-    phoneNumber: '789-012-3456',
-    designation: 'Operations Manager',
-    department: 'Operations',
-    emailAddress: 'chris.white@example.com',
-    image: 'https://i.pravatar.cc/48?u=499476',
-  },
-];
-
-const Card = () => {
-  console.log(employees.length);
+/* eslint-disable react/prop-types */
+const Card = ({
+  onClick,
+  name,
+  employeeCode,
+  index,
+  department,
+  emailAddress,
+  joiningDate,
+  selectedEmployee,
+}) => {
+  let selected;
+  selectedEmployee !== null
+    ? (selected = selectedEmployee.employeeCode === employeeCode)
+    : null;
 
   return (
-    <>
-      {employees.map((e, i) => {
-        return (
-          <div
-            key={e.employeeCode}
-            className=' flex items-center p-6 drop-shadow-lg outline outline-1 outline-LightGrey gap-6 bg-White rounded-lg'
-          >
-            <span className=' h-6 w-6 bg-LightGrey flex items-center justify-center rounded-full p-4 text-MediumGrey'>
-              {i + 1}
-            </span>
-            <div className=' w-[50px] rounded-full overflow-hidden'>
-              <img
-                className='w-full h-auto object-cover'
-                src={`https://i.pravatar.cc/48?${e.employeeCode}`}
-                alt='cus'
-              />
-            </div>
-            <div className=' flex w-full justify-between font-Poppins font-normal text-MediumGrey '>
-              <span className=' basis-[120px] text-[16px] text-Purple font-bold uppercase'>
-                {e.name}
-              </span>
-              <span className=' basis-[130px]'>{e.department}</span>
-              <span className=' basis-2'>{e.employeeCode}</span>
-              <span className='  basis-[230px]'>{e.emailAddress}</span>
-              <span className=' basis-[100px]'>{e.joiningDate}</span>
-            </div>
-          </div>
-        );
-      })}
-    </>
+    <div
+      onClick={() =>
+        onClick({
+          name,
+          employeeCode,
+          index,
+          department,
+          emailAddress,
+          joiningDate,
+        })
+      }
+      key={employeeCode}
+      className={` ${
+        selected
+          ? ' bg-Purple transition-colors duration-200 ease-out'
+          : ' bg-White outline outline-1 outline-LightGrey transition-colors duration-200 ease-in'
+      } flex items-center p-6 drop-shadow-lg  gap-6  rounded-lg select-none  `}
+    >
+      <span
+        className={`${
+          selected ? ' bg-White text-Purple' : ' text-MediumGrey bg-LightGrey'
+        } h-6 w-6 text-sm font-Poppins font-medium flex items-center justify-center rounded-full p-4`}
+      >
+        {index}
+      </span>
+      <div className=' w-[50px] rounded-full overflow-hidden'>
+        <img
+          className='w-full h-auto object-cover'
+          src={`https://i.pravatar.cc/48?${employeeCode}`}
+          alt='cus'
+        />
+      </div>
+      <div
+        className={`${
+          selected ? ' text-LightGrey' : ' text-MediumGrey'
+        } flex w-full justify-between font-Poppins font-normal `}
+      >
+        <span
+          className={` ${
+            selected ? ' text-White' : ' text-Purple'
+          } basis-[120px] text-[16px]  font-bold uppercase`}
+        >
+          {name}
+        </span>
+        <span className=' basis-[130px]'>{department}</span>
+        <span className=' basis-2'>{employeeCode}</span>
+        <span className='  basis-[230px]'>{emailAddress}</span>
+        <span className=' basis-[100px]'>{joiningDate}</span>
+      </div>
+    </div>
   );
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,24 @@
 import Preview from './Preview';
 import EmployeeLists from './EmployeeLists';
 import NewCandidate from './NewCandidate';
+import { useState } from 'react';
 
 const Main = () => {
+  const [selectedEmployee, setselectedEmployee] = useState(null);
+
+  function handleSelectEmployee(employee) {
+    selectedEmployee !== null &&
+    selectedEmployee.employeeCode === employee.employeeCode
+      ? setselectedEmployee(null)
+      : setselectedEmployee(employee);
+  }
+
   return (
     <main className=' flex gap-6 '>
-      <EmployeeLists />
+      <EmployeeLists
+        onSelect={handleSelectEmployee}
+        selectedEmployee={selectedEmployee}
+      />
       <Preview />
       {/* <NewCandidate /> */}
     </main>
